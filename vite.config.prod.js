2e9051a89bd78d5b54763ce40e9c11d229b0c784
Brandon Tassone <brandontassone@gmail.com>
2023-03-20 16:20:10 -0400

    Removed rollup options since vue needs to be included. Updated package.json to 1.0.12

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -17,18 +17,6 @@ export default defineConfig({
       name: 'Minimalist',
       // the proper extensions will be added
       fileName: 'index',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
+    }
   }
 })
